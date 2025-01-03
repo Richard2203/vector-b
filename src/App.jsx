70319ae20e4f54efa3d1d6c0495e1fa875cd6842
Richard10 <logari.1805@gmail.com>
2025-01-03 03:01:54 -0600

    button's tyles coded

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,19 @@ import './App.css'
 function App() {
 
   return (
-    <div id="main-container">
-        
-    </div>
+    <>
+      <div id="main-container">
+      </div>
+      <div id="content">
+        <h1>
+          Analisis de Sensibilidad - Cambios en el Vector B
+        </h1>
+        {/* <button class="button button-calculate" role="button">Button 30</button>
+        <button class="button button-delete" role="button">-</button>
+        <button class="button button-add" role="button">+</button> */}
+      </div>
+    </>
+    
   )
 }
 
