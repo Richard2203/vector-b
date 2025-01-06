b1b2301789e56e5a1f74965eaef9d82899d6f370
Richard10 <logari.1805@gmail.com>
2025-01-06 01:49:12 -0600

    se agregaron estilos ligeros a la tabla

--- src/Matrix/Table.jsx
+++ src/Matrix/Table.jsx
@@ -1,12 +1,7 @@
 import React, { useEffect } from 'react'
 import Fraction from "fraction.js";
 import { useOptimalSolution } from '../hooks/useOptimalSolution';
-
-
-// Función para crear el estado inicial basado en tablaSize 
-function createInitialTableState(variablesNumber, restrictionsNumber) { 
-	
-};
+import './Matrix.css';
 
 const initialTableState = {
   columnas: ["Ecuación", "X1", "X2", "X3", "S1", "S2", "S3", "Sol"],
@@ -44,51 +39,54 @@ export const Table = ({tablaSize}) => {
 		useEffect(() => { 
 			handleUpdateTable(tablaSize[0], tablaSize[1]); 
 		}, [tablaSize]);
-		
-	console.log(tablaSize);
-	console.log(optimalSolution);
 
 	return (
-	  <table border="1">
-		<thead>
-		  <tr>
-			{optimalSolution.columnas.map((columna, index) => (
-			  <th key={index}>{columna}</th>
-			))}
-		  </tr>
-		</thead>
-		<tbody>
-		  {optimalSolution.filas.map((fila, filaIndex) => (
-			<tr key={filaIndex}>
-			  {/* Celda para el tipo e índice */}
-			  <td>
-				<input
-				  type="text"
-				  value={fila.tipo.toString()}
-				  onChange= { (e)=>{ handleUpdateType(filaIndex, e) } }
-				  style={{ width: "50px" }}
-				/>
-				<input
-				  type="number"
-				  value={fila.indice || ""}
-				  onChange={(e) => handleUpdateIndex(filaIndex, e)}
-				  style={{ width: "50px" }}
-				/>
-			  </td>
-			  {/* Celdas para los valores */}
-			  {fila.valores.map((valor, valorIndex) => (
-				<td key={valorIndex}>
-				  <input
-					type="text"
-					value={valor}
-					onChange={(e) => handleUpdateValue(filaIndex, valorIndex, e)}
-					style={{ width: "80px" }}
-				  />
-				</td>
-			  ))}
-			</tr>
-		  ))}
-		</tbody>
+	 <>
+			<button 
+				type="button" 
+				className='button button-calculate'
+				onClick = { ()=> {} }> Calcular </button>
+			<table border="1">		
+			<thead>
+				<tr>
+				{optimalSolution.columnas.map((columna, index) => (
+					<th key={index}>{columna}</th>
+				))}
+				</tr>
+			</thead>
+			<tbody>
+				{optimalSolution.filas.map((fila, filaIndex) => (
+				<tr key={filaIndex}>
+					{/* Celda para el tipo e índice */}
+					<td>
+						<input
+							type="text"
+							value={fila.tipo.toString()}
+							onChange= { (e)=>{ handleUpdateType(filaIndex, e) } }
+							style={{ width: "50px" }}
+						/>
+						<input
+							type="number"
+							value={fila.indice || ""}
+							onChange={(e) => handleUpdateIndex(filaIndex, e)}
+							style={{ width: "50px" }}
+						/>
+					</td>
+					{/* Celdas para los valores */}
+					{fila.valores.map((valor, valorIndex) => (
+					<td key={valorIndex}>
+						<input
+						type="text"
+						value={valor}
+						onChange={(e) => handleUpdateValue(filaIndex, valorIndex, e)}
+						style={{ width: "80px" }}
+						/>
+					</td>
+					))}
+				</tr>
+				))}
+			</tbody>
 	  </table>
+	 </>
 	);
 };
